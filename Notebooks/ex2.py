dbf1f004f90e7590ec190e2203475a2cccfcee52
Christophe Morisset <chris.morisset@gmail.com>
2025-02-11 09:47:18 -0600

    before school

---
+++ Notebooks/ex2.py
@@ -0,0 +1,13 @@
+#%% imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%% functions
+def fun(x):
+    return x**2
+#%% plot
+f, ax = plt.subplots(1, 1)
+x = np.linspace(-10, 10, 100)
+y = fun(x)
+ax.plot(x, y)
+#%%
