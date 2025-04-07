394b36abdf074a632c9cc2238a89948cca9fcefd
Christophe Morisset <chris.morisset@gmail.com>
2025-04-07 16:33:52 -0600

    rename directory

--- Notebooks/ex1.py
+++
@@ -1,8 +0,0 @@
-# This write the current cell to a file
-#%%
-def f1(x):
-    """
-    This is an example of a function, returning x**2
-    - parameter: x
-    """
-    return x**2
--- Notebooks/fact.py
+++
@@ -1,3 +0,0 @@
-import sys
-import numpy as np
-print(np.math.factorial(int(sys.argv[1])))
